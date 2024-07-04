4b2a93748318e7823df5dd74a78accc15f82b74f
Soumalya Majumdar <soumalyamajumdar18@gmail.com>
2024-07-05 01:13:03 +0530

    implemented DFS for solving

---
+++ src/DFSSolver.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Typename T: RubiksCube Representation used (3D, 1D, Bitboard)
+// Typename H: Corresponding Hash function (could be used in more advanced methods)
+
+public class DFSSolver<T extends RubiksCube> {
+    private final List<RubiksCube.Move> moves;
+    private final int maxSearchDepth;
+    private final T rubiksCube;
+
+    // Constructor
+    public DFSSolver(T rubiksCube, int maxSearchDepth) {
+        this.rubiksCube = rubiksCube;
+        this.maxSearchDepth = maxSearchDepth;
+        this.moves = new ArrayList<>();
+    }
+
+    // DFS method to find the solution
+    private boolean dfs(int depth) {
+        if (rubiksCube.isSolved()) {
+            return true;
+        }
+        if (depth > maxSearchDepth) {
+            return false;
+        }
+        for (int i = 0; i < 18; i++) {
+            rubiksCube.performMove(RubiksCube.Move.values()[i]);
+            moves.add(RubiksCube.Move.values()[i]);
+            if (dfs(depth + 1)) {
+                return true;
+            }
+            moves.removeLast();
+            rubiksCube.invert(RubiksCube.Move.values()[i]);
+        }
+        return false;
+    }
+
+    // Solve method to initiate DFS and return the solution
+    public List<RubiksCube.Move> solve() {
+        dfs(1);
+        return moves;
+    }
+
+}
